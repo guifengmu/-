d255b11157aed81f2467301cb6449154b4562f4c
guifengmu <39817491+guifengmu@users.noreply.github.com>
2018-06-14 14:21:43 +0800

    Create 删除序列相同元素.py

---
+++ 删除序列相同元素.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+a = [ {'x':1, 'y':2}, {'x':1, 'y':3}, {'x':1, 'y':2}, {'x':2, 'y':4}]
+
+def deque(items,key=None):
+  seen=set()
+   for item in items:
+    val=item if key is None else key(item)
+    if val not in seen:
+      yield item
+      seen.add(val)
+
+b= list(deque(a,lambda d:(d['x'],d['y'])))
+
+c = list(deque(a,lambda d:d['x']))
