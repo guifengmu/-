060e0c999e6e4b6cf7b0ccd73aadd2f196d464fd
guifengmu <39817491+guifengmu@users.noreply.github.com>
2018-06-14 15:31:08 +0800

    Create 以字典关键字排序.py

---
+++ 以字典关键字排序.py
@@ -0,0 +1,13 @@
+#!/use/bin/env python3
+#-*-coding: utf-8 -*-
+
+from collections import itemgetter
+
+rows = [
+{'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
+{'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
+{'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
+{'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
+]
+
+rows_by_name=sorted(rows,key=itemgetter('fname'))
