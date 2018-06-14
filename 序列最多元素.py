379fda4d52e2d005d38307f2ebbd5cdcb7b5f3cd
guifengmu <39817491+guifengmu@users.noreply.github.com>
2018-06-14 14:49:50 +0800

    Create 序列最多元素.py

---
+++ 序列最多元素.py
@@ -0,0 +1,22 @@
+#!/usr bin/env python3
+#-*- coding: utf-8-*-
+
+from collections import Counter
+
+words = [
+'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
+'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
+'my', 'eyes', "you're", 'under'
+]
+
+word_counts=Counter(words)
+
+top_three= word_counts.most_common
+
+print(top_three)
+
+for key in words:
+  words_count[key]+=1
+  
+  
